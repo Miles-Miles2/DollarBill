2e3cb54a2a883120eb8bc9d6e5712760f9720f19
MIlesK <miles.keiser@gmail.com>
2023-07-07 13:50:33 -0700

    Initial Commit

---
+++ DollarBill/DollarBillApp.swift
@@ -0,0 +1,17 @@
+//
+//  DollarBillApp.swift
+//  DollarBill
+//
+//  Created by Keiser on 7/7/23.
+//
+
+import SwiftUI
+
+@main
+struct DollarBillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
